d3dccacce2facb90102e77b499d3131b37a88338
Daniel La Rocque <64338275+dlarocque@users.noreply.github.com>
2020-07-01 19:31:06 -0500

    Add files via upload
    Fixed the issue!
    The program now shows the array being sorted
    Had to learn about threads and SwingWorker.

--- AlgVisualiser.java
+++ AlgVisualiser.java
@@ -1,8 +1,6 @@
 /*
-**-dont use preferred sizes for everything
-**-don't use static for everything
-**-add getters and setters
-**-no need for revalidate
+    add an action Listener to space bar to reset program
+    add text on paintComponent to tell this to the user
 */
 
 import java.awt.Color;
@@ -19,58 +17,55 @@ public class AlgVisualiser implements ActionListener {
     final int CONTENT_WIDTH = 800;
     final int CONTENT_HEIGHT = 800;
     JFrame frame = new JFrame("Sorting Algorithms");
+    DisplayArr display;
     JPanel buttonPanel = new JPanel();
-    JPanel arrPanel = new JPanel();
     JButton bubbleButton;
     JButton insertionButton;
     JButton selectionButton;
-    Bubble bubbleSort;
-    Insertion insertSort;
-    Selection selectionSort;
+    SwingWorker<Void, Integer[]> bubbleWorker;
+    private boolean doBubbleSort;
+    private boolean doInsertionSort;
+    private boolean doSelectionSort;
 
     public static void main(String[] args) {
         AlgVisualiser alg = new AlgVisualiser();
         alg.initializeVars();
         alg.setFrame();
+
     }
 
     public void setFrame() {
         frame.setLayout(new CardLayout());
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setPreferredSize(new Dimension(CONTENT_WIDTH, CONTENT_HEIGHT));
-        frame.setLocationRelativeTo(null);
+        //frame.getContentPane().setPreferredSize(new Dimension(CONTENT_WIDTH, CONTENT_HEIGHT));
         buttonPanel.setVisible(true);
         frame.add(buttonPanel);
-        frame.add(arrPanel);
+        frame.add(display);
         frame.pack();
+        frame.setLocationRelativeTo(null);
     }
 
     public void initializeVars() {
+
+        doBubbleSort = false;
+        doInsertionSort = false;
+        doSelectionSort = false;
 
         arr = new Integer[N];
         arr = fillArr(arr);
         arr = shuffleArr(arr);
 
-        bubbleSort = new Bubble(this, arr);
-        bubbleSort.setPreferredSize(new Dimension(CONTENT_WIDTH, CONTENT_HEIGHT));
-
-        insertSort = new Insertion(this, arr);
-        insertSort.setPreferredSize(new Dimension(CONTENT_WIDTH, CONTENT_HEIGHT));
-
-        selectionSort = new Selection(this, arr);
-        selectionSort.setPreferredSize(new Dimension(CONTENT_WIDTH, CONTENT_HEIGHT));
+        display = new DisplayArr(this, arr);
+        display.setPreferredSize(new Dimension(CONTENT_WIDTH, CONTENT_HEIGHT));
 
         bubbleButton = new JButton("Bubble Sort");
-        bubbleButton.setPreferredSize(new Dimension(200, 200));
         bubbleButton.addActionListener(this);
 
         selectionButton = new JButton("Selection Sort");
-        selectionButton.setPreferredSize(new Dimension(200, 200));
         selectionButton.addActionListener(this);
 
         insertionButton = new JButton("Insertion Sort");
-        insertionButton.setPreferredSize(new Dimension(200, 200));
         insertionButton.addActionListener(this);
 
         bubbleButton.setBackground(Color.WHITE);
@@ -78,29 +73,35 @@ public class AlgVisualiser implements ActionListener {
         insertionButton.setBackground(Color.WHITE);
 
         buttonPanel.setBackground(Color.DARK_GRAY);
-        buttonPanel.setPreferredSize(new Dimension(CONTENT_WIDTH, CONTENT_HEIGHT));
         buttonPanel.add(bubbleButton);
         buttonPanel.add(selectionButton);
         buttonPanel.add(insertionButton);
 
-        arrPanel.setPreferredSize(new Dimension(CONTENT_WIDTH, CONTENT_HEIGHT));
-        arrPanel.add(bubbleSort);
+        bubbleWorker = new bubbleWorker(this, arr, display);
+
     }
 
     public void actionPerformed(ActionEvent event) {
 
+        doBubbleSort = false;
+        doSelectionSort = false;
+        doInsertionSort = false;
+
         if (event.getSource() == bubbleButton) {
             buttonPanel.setVisible(false);
-            arrPanel.setVisible(true);
-            bubbleSort.sort();
+            display.setVisible(true);
+            doBubbleSort = true;
+            bubbleWorker.execute();
         } else if (event.getSource() == selectionButton) {
             buttonPanel.setVisible(false);
-            arrPanel.setVisible(true);
-            insertSort.sort();
+            display.setVisible(true);
+            doSelectionSort = true;
+            bubbleWorker.execute();
         } else if (event.getSource() == insertionButton) {
             buttonPanel.setVisible(false);
-            arrPanel.setVisible(true);
-            selectionSort.sort();
+            display.setVisible(true);
+            doInsertionSort = true;
+            bubbleWorker.execute();
         }
 
     }
@@ -134,5 +135,26 @@ public class AlgVisualiser implements ActionListener {
 
     public JFrame getJFrame() {
         return frame;
+    }
+
+    public void setSort(String algorithm) {
+        if (algorithm.equals("Bubble Sort")) {
+            doBubbleSort = true;
+        } else if (algorithm.equals("Insertion Sort")) {
+            doInsertionSort = true;
+        } else if (algorithm.equals("Selection Sort")) {
+            doSelectionSort = true;
+        }
+    }
+
+    public String getSort() {
+        String algorithm = null;
+        if (doBubbleSort)
+            algorithm = "Bubble Sort";
+        if (doInsertionSort)
+            algorithm = "Insertion Sort";
+        if (doSelectionSort)
+            algorithm = "Selection Sort";
+        return algorithm;
     }
 }
